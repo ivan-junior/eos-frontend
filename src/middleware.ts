9aec8da101e23a8cca5274f1e12037c52d5d0327
Ivan Junior <ivanbjunior.93@gmail.com>
2023-12-10 23:25:12 -0300

    Fixed middleware for build

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,8 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(request: NextRequest) {
 	const token = request.cookies.get('token')?.value
 	const pathName = request.nextUrl.pathname
-	const protectedPaths = ['/feed', '/post', '/profile']
-	const isPathProtected = protectedPaths.some((path) => pathName.startsWith(path))
-
-	if (!token) {
-		const url = new URL('/login', request.url)
-		return NextResponse.redirect(url)
-	}
+	const protectedPaths = ['/feed', '/post', '/profile', '/admin']
+	const isPathProtected = protectedPaths.some((path) => pathName == path)
 
 	if (isPathProtected) {
 		if (!token) {
